698c9508a01be44afc333bd8e55ae879bf446a83
umair-bandlab <umairafzal10@yahoo.com>
2025-01-02 16:06:45 +0500

    Added tableView register method

--- Sources/IOS Helpers/Extensions/UIKit/UITableView+Extension.swift
+++ Sources/IOS Helpers/Extensions/UIKit/UITableView+Extension.swift
@@ -4,6 +4,10 @@ import UIKit
 
 public extension UITableView {
     
+    func register<T: UITableViewCell>(_: T.Type, indexPath: IndexPath) {
+        self.register(UINib(nibName: String(describing: T.self), bundle: .main), forCellReuseIdentifier: String(describing: T.self))
+    }
+    
     func register<T: UITableViewCell>(_: T.Type, indexPath: IndexPath) -> T {
         self.register(UINib(nibName: String(describing: T.self), bundle: .main), forCellReuseIdentifier: String(describing: T.self))
         let cell = self.dequeueReusableCell(withIdentifier: String(describing: T.self), for: indexPath) as! T
